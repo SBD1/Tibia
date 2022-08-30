30787c01c01336d398977628459bcb07be33bd3b
jvsdurso <jvsdurso@gmail.com>
2022-08-29 23:23:19 -0300

    Retificando as funções

--- docs/AlgebraRelacional/storedprocedures.sql
+++ docs/AlgebraRelacional/storedprocedures.sql
@@ -4,7 +4,8 @@ CREATE OR REPLACE FUNCTION get_player_level(_id_player INTEGER)
     DECLARE
         xp_player BIGINT;
 BEGIN
-  RETURN (SELECT experiencia INTO xp_player FROM player WHERE id=_id_player) / 1000;
+	SELECT experiencia INTO xp_player FROM player WHERE id=_id_player;
+  	RETURN ((xp_player) / 1000) + 1;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -20,41 +21,50 @@ END;
 $$ LANGUAGE plpgsql;
 
 --- Validar a passagem para nova regiao
-CREATE OR REPLACE FUNCTION valid_region_change(_id_posicao INTEGER, _id_player INTEGER, _vip_check BOOLEAN, _id_conta INTEGER, _vip_dias SMALLINT)
+CREATE OR REPLACE FUNCTION valid_region_change(_id_posicao INTEGER, _id_player INTEGER)
   RETURNS BOOLEAN AS $$
 DECLARE
-  _regiao_vip BOOLEAN;
-  _player_vip SMALLINT;
   _permitido BOOLEAN;
+  _vip_check BOOLEAN;
+  _id_conta INTEGER;
+  _vip_dias SMALLINT;
+  
 BEGIN
-  _regiao_vip := (SELECT r.vip_check FROM regiao r WHERE r.id = (SELECT p.id_regiao FROM localizao WHERE p.id = _id_posicao));
-  IF vip = FALSE THEN
-    _permitido := TRUE;
-  ELSE 
-    _player_vip := (SELECT d.vip_dias FROM conta d WHERE d.id = (SELECT c.id_conta FROM conta WHERE c.id = _id_conta));
-    IF _player_vip > 0 THEN
-      _permitido = TRUE;
-    ELSE
-      _permitido = FALSE;
+	_vip_check := (SELECT vip_check FROM regiao INNER JOIN localizacao ON regiao.id = localizacao.id_regiao WHERE localizacao.id = _id_posicao);
+	_id_conta := (SELECT id_conta FROM conta INNER JOIN player ON player.id_conta = conta.id WHERE player.id = _id_player);
+	IF _vip_check = FALSE THEN
+		_permitido := TRUE;
+	ELSE 
+		_vip_dias := (SELECT vip_dias FROM conta INNER JOIN player ON player.id_conta = conta.id WHERE player.id = _id_player);
+		IF _vip_dias > 0 THEN
+			_permitido = TRUE;
+		ELSE
+			_permitido = FALSE;
+		END IF;
 	END IF;
-  END IF;
-    RETURN _permitido;
+	RETURN _permitido;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Função que retorna o id do item a partir do id da instância do item
-CREATE OR REPLACE FUNCTION get_id_item (_id_instancia INTEGER)
-  RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION get_id_item(_id_instancia INTEGER)
+	RETURNS INTEGER AS $$
+	DECLARE
+  	_id_item INTEGER;
 BEGIN
-  RETURN (SELECT id_item FROM instancia_item WHERE id=_id_instancia);
+	_id_item := (SELECT id_item FROM instancia_item WHERE id=_id_instancia);
+	RETURN _id_item;
 END;
 $$ LANGUAGE plpgsql;
 
 --- Atributo posição do player
 CREATE OR REPLACE FUNCTION get_player_position(_id_player INTEGER)
   RETURNS INTEGER AS $$
+  DECLARE 
+  	_id_posicao INTEGER;
 BEGIN
-  RETURN (SELECT id_posicao FROM player WHERE id = _id_player);
+	_id_posicao := (SELECT id_localizacao FROM personagem INNER JOIN player ON player.idpersonagem = personagem.id WHERE player.id = _id_player);
+  	RETURN _id_posicao;
 END;
 $$ LANGUAGE plpgsql;
 
